3d781c41d5f33d8904611f45d72ee5b5056bda0e
James Rezendes <James@MacBook-Pro.local>
2017-11-01 15:18:48 -0700

    User Authentication Form and Routes Added.

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var flash = require('connect-flash');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var maps = require('@google/maps').createClient({
     key: process.env.MAP_API_KEY
@@ -18,27 +21,31 @@ mongoose.connect(process.env.MONGO_URL, function(err){
     console.log('Connected to Tips database.');
 });
 
-// required for passport
-require('./controllers/passport')(passport);
-
 // create .env file if not present, assign a value to SESSION_SECRET
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 app.use(session({secret: process.env.SESSION_SECRET})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
+app.use(flash());
 app.use(express.static('./public')); // set directory for static files
+
 
 
 app.set('views', './views'); // set express view template directory for express
 app.set('view engine' , 'jade'); // set express view engine to use jade
 
 app.get('/', function (req, res) {
-    res.render('index')
+    req.flash('info', 'Welcome');
+    res.render('index', {currentUser : req.user, infoFlash : req.flash('info')})
 });
 
+// required for passport
+require('./controllers/passport')(passport);
+//Routes for authentication
+require('./controllers/auth')(app, passport);
 // Routes for Tips
 require('./controllers/tips')(app);
-// Routes for Authentication
-require('./controllers/auth')(app, passport);
 
 app.listen(port, function () {
     console.log('Awesome tips listening on port 3000!')
